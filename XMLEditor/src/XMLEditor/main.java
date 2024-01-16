cd46ee3d56286b58c52717d5c6e0b332ef801efe
David <131477016+daavidmeeejia@users.noreply.github.com>
2024-01-16 16:53:01 +0100

    main.java

--- XMLEditor/src/XMLEditor/main.java
+++ XMLEditor/src/XMLEditor/main.java
@@ -3,24 +3,17 @@ package XMLEditor;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-
-import org.w3c.dom.Attr;
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
